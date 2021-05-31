df6900c1fe66e5f528ef8a3fc62bd663786abba4
Joao <joaoricardotg@gmail.com>
2021-05-31 17:37:45 -0300

    Update app.js

--- backend-postgres/app.js
+++ backend-postgres/app.js
@@ -4,7 +4,8 @@ const cors = require("cors");
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
-// nao utilizar em producao. compromete a seguranca da aplicacao
+// do not use in production
+// bypasses SSL/TSL verification
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 const app = express();
@@ -13,13 +14,15 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const pool = new Pool({
-  user: "jvanggcqbsrbtt",
-  host: "ec2-3-214-136-47.compute-1.amazonaws.com",
-  database: "ddktvhalkn4d1g",
-  password: "621daa6f2ada94f59fc6f7131aef99968d07cbb56900a90fd4f8d96b4492c151",
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.DB_PASSWORD,
   port: 5432,
   ssl: true,
 });
+
+// break these 3 routes into their own .js files
 
 // pesquisa os dados do usuario pelo seu id
 app.get("/getUser/:id", (req, res) => {
